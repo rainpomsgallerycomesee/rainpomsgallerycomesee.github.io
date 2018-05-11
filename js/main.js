9c08cae728d58bda3f3c318ffd5881e252ef4646
rainpomsgallerycomesee <39179332+rainpomsgallerycomesee@users.noreply.github.com>
2018-05-10 20:56:17 -0700

    upload files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//   $(".hamburger, .close").click(function(){
+//     $(".main-nav").toggleClass("open-nav");
+// });
+// $(".img").click(function(){
+//  $(".img").toggleClass("imgfullscreen");
+//
+// });
+// });
